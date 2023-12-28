66f5aed978cd6aecf2477891e5561d8ae33c69cb
Yash Kumar <Yashxhere@gmail.com>
2023-12-28 13:59:51 +0530

    Input

---
+++ Input.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Input {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        
+        String input = sc.next();
+        System.out.println(input);
+    //Here everything after space is left whenever we use sc.next!
+
+        String name = sc.nextLine();
+        System.out.println(name);
+    //Here Space is included while writing the string whenever we use sc.nextLine!
+
+        int number = sc.nextInt();
+        System.out.println(number);
+    }
+}
--- Sum.java
+++ Sum.java
@@ -2,7 +2,7 @@ public class Sum {
     public static void main(String args[]){
         int a = 53;
         int b = 77;
-        int sum = a+b;
+        int sum = a + b;
         System.out.println(sum);
     }
 }
