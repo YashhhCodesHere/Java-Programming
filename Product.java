eda14b4cb0882a2ae9e922dfce554067bc079679
Yash Kumar <Yashxhere@gmail.com>
2023-12-31 02:32:23 +0530

    Adding Changes

--- Product.java
+++ Product.java
@@ -3,9 +3,19 @@ import java.util.*;
 public class Product {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the value of a: ");
         int a = sc.nextInt();
+        System.out.print("Enter the value of b: ");
         int b = sc.nextInt();
+        int sum = a + b;
+        int difference = a - b;
         int product = a * b;
-        System.out.println(product);
+        int quotient = a / b;
+        int remainder = a % b;
+        System.out.println("\nThe sum of a and b is: " + sum);
+        System.out.println("\nThe difference of a and b is: " + difference);
+        System.out.println("\nThe product of a and b is: " + product);
+        System.out.println("\nThe quotient of a and b is: " + quotient);
+        System.out.println("\nThe remainder obtained when a and b is divided: " + remainder);
     }
 }
--- TypeConversion.java
+++ TypeConversion.java
@@ -3,6 +3,8 @@ import java.util.*;
 public class TypeConversion {
     public static void main(String args[]){
         int a = 20; 
+        long b = a;
+        System.out.println("The value of b is: " + b);
         
     }
 }
