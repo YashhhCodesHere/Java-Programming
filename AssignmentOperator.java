22dd7635ad50d58c30a215b81366d0aa9bb0b3bb
Yash Kumar <Yashxhere@gmail.com>
2024-01-02 04:32:09 +0530

    Some Change to Readme file

---
+++ AssignmentOperator.java
@@ -0,0 +1,14 @@
+public class AssignmentOperator {
+    public static void main(String args[]){
+        int a = 35;
+        System.out.println(a);
+        a += 15;
+        System.out.println(a);
+        a -= 20;
+        System.out.println(a);
+        a *= 10;
+        System.out.println(a);
+        a /= 5;
+        System.out.println(a);
+    }
+}
