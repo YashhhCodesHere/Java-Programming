bc0bf394ee87b94ab6cab2ff3a691e950d85c7f9
Yash Kumar <Yashxhere@gmail.com>
2024-01-09 22:49:52 +0530

    Ternary Operator

---
+++ TernaryOperator.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class TernaryOperator {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your desired number: ");
+        int number = sc.nextInt();
+        String OddEven;
+        OddEven = ((number%2)==0) ? "EVEN" : "ODD";
+        System.out.println("Your entered number is: " + OddEven);
+    }
+}
