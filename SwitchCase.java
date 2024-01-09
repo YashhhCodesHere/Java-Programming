c952fad19d5b22d34ed0309c2c4c3af1510d177a
Yash Kumar <Yashxhere@gmail.com>
2024-01-09 23:47:14 +0530

    Some changes

--- SwitchCase.java
+++ SwitchCase.java
@@ -1,5 +1,16 @@
 import java.util.*;
 
 public class SwitchCase {
-    
+    public static void main(String args[]){
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter 1, 2, or 3: ");
+    int number = sc.nextInt();
+
+    switch(number) {
+        case 1 : System.out.println("A");
+        case 2 : System.out.println("B");
+        case 3 : System.out.println("C");
+        default : System.out.println("The value you entered doesn't lies in our range!");
+    }
+  }
 }
