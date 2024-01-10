fe9a6d25992d3758d59f659f1bae01f4e4a8aa30
Yash Kumar <Yashxhere@gmail.com>
2024-01-10 22:28:18 +0530

    Print 1 to n where n is entered by user

---
+++ Print1toN.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Print1toN {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number upto which you want to be printed: ");
+        int number = sc.nextInt();
+        int counter = 1;
+
+        while(counter <= number){
+            System.out.println(counter);
+            counter++;
+        }
+
+    }
+
+}
