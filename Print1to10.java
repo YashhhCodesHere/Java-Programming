c7a956c0e751fda4ab130204744124ccfeee2674
Yash Kumar <Yashxhere@gmail.com>
2024-01-10 22:23:50 +0530

    Some Changes

--- Print1to10.java
+++ Print1to10.java
@@ -1,8 +1,5 @@
-import java.util.*;
-
 public class Print1to10 {
     public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
         int counter = 1;
 
         while (counter <= 10) {
