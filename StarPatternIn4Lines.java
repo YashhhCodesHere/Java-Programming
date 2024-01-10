1e2862afb27aaa091ea693bc2e1e97497c957aa5
Yash Kumar <Yashxhere@gmail.com>
2024-01-10 23:37:04 +0530

    Some Modification In Star Pattern By both For and While loop

--- StarPatternIn4Lines.java
+++ StarPatternIn4Lines.java
@@ -4,9 +4,20 @@ public class StarPatternIn4Lines {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         String StarPattern = "|* * * *|";
+
+        System.out.println("\nThe star pattern printed by For loop: \n");
         
         for(int i = 1; i <= 4; i++){
             System.out.println(StarPattern);
+        }
+        System.out.println("\n");
+        //Same can be done by while loop also!
+        System.out.println("The star pattern printed by While loop: \n");
+
+        int j = 1;
+        while(j <= 4){
+            System.out.println(StarPattern);
+            j++;
         }
     }
 }
