b0084869d280d06031fe5727f3fe8cdec577c10a
Yash Kumar <Yashxhere@gmail.com>
2024-01-10 22:45:26 +0530

    file deleted

--- SumOfN.java
+++ SumOfN.java
@@ -1,5 +1,15 @@
 import java.util.*;
 
 public class SumOfN {
-    sttc
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int start = 1;
+        int sum;
+        int counter = sc.nextInt();
+        while (start <= counter) {
+            sum = start + 1;
+            start++;
+        }
+        System.out.println(sum);
+    }
 }
