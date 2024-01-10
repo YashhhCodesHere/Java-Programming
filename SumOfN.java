bcbea692be51bb87ffc4a572b00d61a525fc4652
Yash Kumar <Yashxhere@gmail.com>
2024-01-10 23:00:55 +0530

    Sum of the numbers from 1 to N

--- SumOfN.java
+++ SumOfN.java
@@ -3,13 +3,14 @@ import java.util.*;
 public class SumOfN {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        int start = 1;
-        int sum;
+        System.out.print("Enter the value of 'N': ");
         int counter = sc.nextInt();
+        int start = 1;
+        int sum = 0;
         while (start <= counter) {
-            sum = start + 1;
+            sum += start;
             start++;
         }
-        System.out.println(sum);
+        System.out.println("The sum of 1 to N is: " +sum);
     }
 }
