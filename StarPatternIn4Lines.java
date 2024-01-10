c4b11745827b68a78286563309eaa38367205135
Yash Kumar <Yashxhere@gmail.com>
2024-01-10 23:30:51 +0530

    Star Pattern 4 times

---
+++ StarPatternIn4Lines.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class StarPatternIn4Lines {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        String StarPattern = "|* * * *|";
+        
+        for(int i = 1; i <= 4; i++){
+            System.out.println(StarPattern);
+        }
+    }
+}
