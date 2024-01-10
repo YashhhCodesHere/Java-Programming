7435724ec1da778a053fdf43bb89a3ed83c88363
Yash Kumar <Yashxhere@gmail.com>
2024-01-11 00:04:51 +0530

    Reverse of any number entered by user

---
+++ ReverseOfNumber.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class ReverseOfNumber {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the no. you want the reverse of: ");
+        int original = sc.nextInt();
+        
+        while(original > 0){
+        System.out.print(original % 10); //To get the last digit of the number
+        original = original / 10; //To remove the last digit of the number
+        }
+    }
+}
