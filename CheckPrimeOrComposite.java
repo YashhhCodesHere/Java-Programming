7732249a1f363442681b21a3070510000bcdf3a6
Yash Kumar <Yashxhere@gmail.com>
2024-01-16 19:37:18 +0530

    Alternate way added

--- CheckPrimeOrComposite.java
+++ CheckPrimeOrComposite.java
@@ -16,3 +16,25 @@ public class CheckPrimeOrNot {
         }
     }
 }
+
+//Alternate code for the above:-
+// import java.util.*;
+
+// public class CheckPrimeOrNot{
+//     public static void main(String args[]){
+//         Scanner sc = new Scanner(System.in);
+//         System.out.print("Enter your number: ");
+//         int n = sc.nextInt();
+//         boolean isPrime = true;
+//         for (int i = 2; i <= (n-1); i++){
+//             if((n%i) == 0){
+//                 isPrime = false;
+//             }
+//         }
+//         if(isPrime == false){
+//             System.out.println("Your entered no. is composite!");
+//         }else{
+//             System.out.println("Your entered no. is prime!");
+//         }
+//     }
+// }
