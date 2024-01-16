27ece0dc8d02670c7d11932af7e399f341c2cc43
Yash Kumar <Yashxhere@gmail.com>
2024-01-16 21:16:57 +0530

    Syntax of Square Root

---
+++ SquareRootSyntax.java
@@ -0,0 +1,7 @@
+public class SquareRootSyntax {
+    public static void main(String args[]){
+        int n = 4;
+        System.out.println(Math.sqrt(n));
+        //The above Syntax take square root as input!
+    }
+}
