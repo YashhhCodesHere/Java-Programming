23d6c6617707496376d11f8e3c25e18ebba74da5
Yash Kumar <Yashxhere@gmail.com>
2024-01-16 19:07:18 +0530

    Check either the number is prime or not!

---
+++ CheckPrimeOrComposite.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class CheckPrimeOrNot {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        do{
+            System.out.println("Enter your number to check either it's Prime or NOT: ");
+            int n = sc.nextInt();
+            if()
+        }
+    }
+}
