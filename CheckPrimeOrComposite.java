d7b6f8692f5724743a0e4a5318190963d5ac1f1e
Yash Kumar <Yashxhere@gmail.com>
2024-01-16 19:40:02 +0530

    some mods

--- CheckPrimeOrComposite.java
+++ CheckPrimeOrComposite.java
@@ -5,15 +5,20 @@ public class CheckPrimeOrNot {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter your number to check either it's Prime or NOT: ");
         int n = sc.nextInt();
-        for(int i = 2; i <= (n - 1) ; i++){
-            if(n % i == 0){
-                System.out.println("You've entered a Composite number!");
-                break;
-            }else{
-                System.out.println("You've entered a Prime number!");
-                break;
+        if(n == 2){
+            System.out.println("The number is prime!");
+        }else{
+            for(int i = 2; i <= (n - 1) ; i++){
+                if(n % i == 0){
+                    System.out.println("You've entered a Composite number!");
+                    break;
+                }else{
+                    System.out.println("You've entered a Prime number!");
+                    break;
+                }
             }
         }
+       
     }
 }
 
