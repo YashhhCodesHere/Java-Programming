f24bf0ddbee5ae0be0a72cb580ca842c38f1a4d4
Yash Kumar <Yashxhere@gmail.com>
2024-01-17 10:47:34 +0530

    Another formula based approach added

--- InvertedStarPattern.java
+++ InvertedStarPattern.java
@@ -1,10 +1,25 @@
+import java.util.*;
+
 public class InvertedStarPattern {
     public static void main(String args[]){
-        for(int line = 1; line <= 4; line++){
-            for(int star = 4; star >= line; star--){
+        int n = 5;
+
+        for(int line = 1; line <= n; line++){
+            for(int star = 1; star <= (n - line + 1); star++){
                 System.out.print("* ");
             }
             System.out.println();
         }
     }
 }
+
+
+
+
+// Another Concept:-
+// for(int line = 1; line <= 4; line++){
+//     for(int star = 4; star >= line; star--){
+//         System.out.print("* ");
+//     }
+//     System.out.println();
+// }
