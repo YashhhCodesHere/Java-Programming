8677c4efcc7c0789ef4fb5307c9af3905c81db68
Yash Kumar <Yashxhere@gmail.com>
2024-01-20 14:32:41 +0530

    Added Comments

--- Functions.java
+++ Functions.java
@@ -1,10 +1,12 @@
 public class Functions {
+    //We must have to add return whenever it's return type is other than void!
     public static void printHelloWorld() {
         System.out.println("Hello World");
         System.out.println("Hello World");
         System.out.println("Hello World");
         System.out.println("Hello World");
         System.out.println("Hello World");
+        return;
     }
     public static void main(String args[]) {
         System.out.println("Now we're going to print Hello World 5 times by calling a function...\n");
