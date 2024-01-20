211b033d2fde545175dfe7c19564945d2f92fbcc
Yash Kumar <Yashxhere@gmail.com>
2024-01-20 19:46:47 +0530

    Another way to multiply by returning product value

---
+++ Multiply2NoByFunction.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Multiply2NoByFunction {
+    public static int multiply(int a, int b){
+        int product = (a * b);
+        return product;
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your no: ");
+        int a = sc.nextInt();
+        System.out.print("Enter your no: ");
+        int b = sc.nextInt();
+        int product = multiply(a, b);
+        System.out.println("Product of your numbers are: " + product);
+    }
+}
