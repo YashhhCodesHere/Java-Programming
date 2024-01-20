3254ae13c93bd2136758a8962099ca1993a223cf
Yash Kumar <Yashxhere@gmail.com>
2024-01-20 14:27:45 +0530

    5 times Hello World

--- Functions.java
+++ Functions.java
@@ -1,6 +1,13 @@
-
 public class Functions {
-    public static void main(String args[]){
-        
+    public static void printHelloWorld() {
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+    }
+    public static void main(String args[]) {
+        System.out.println("Now we're going to print Hello World 5 times by calling a function...\n");
+        printHelloWorld();
     }
 }
