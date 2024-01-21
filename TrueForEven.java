4d76150e9cca25386bc5e249f62566090ece24bb
Yash Kumar <Yashxhere@gmail.com>
2024-01-22 04:46:00 +0530

    Even or Odd

--- TrueForEven.java
+++ TrueForEven.java
@@ -1,7 +1,22 @@
 import java.util.*;
 
 public class TrueForEven {
+    public static boolean isEven(int evenOdd){
+        boolean isEven = true;
+        if(evenOdd % 2 == 1){
+            isEven = false;
+        }
+        return isEven;
+    }
     public static void main(String args[]){
-    
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter your number: ");
+    int evenOdd = sc.nextInt();
+    isEven(evenOdd);
+    if(isEven(evenOdd)){
+        System.out.println("Your entered number " + evenOdd + " is Even!");
+    } else {
+        System.out.println("Your entered number " + evenOdd + " is Odd!");
+    }
     }
 }
