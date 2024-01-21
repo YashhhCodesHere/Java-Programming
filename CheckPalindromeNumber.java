bf6164a515305230dfa6790c631e58141732c7cc
Yash Kumar <Yashxhere@gmail.com>
2024-01-22 05:16:44 +0530

    Checking Palindrome number

--- CheckPalindromeNumber.java
+++ CheckPalindromeNumber.java
@@ -1,8 +1,25 @@
 import java.util.*;
 
 public class CheckPalindromeNumber {
+    public static void isPalindrome(int input){
+        int originalNum = input;
+        int reverse = 0;
+        while (originalNum > 0){
+            int lastDigit = (originalNum % 10);
+            reverse = (reverse * 10) + lastDigit;
+            originalNum /= 10;
+        }
+        System.out.println("Reverse of your number is: " + reverse);
+        if(input == reverse){
+            System.out.println(input + " is Palindrome number!");
+        } else if (input != reverse) {
+            System.out.println(input + " is Not a Palindrome number!");
+        }
+    }
     public static void main(String args[]){
-    Scanner sc = new Scanner(System.in);
-    System.out.println();
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your number to check it's Palindrome or NOT: ");
+        int input = sc.nextInt();
+        isPalindrome(input);
     }
 }
