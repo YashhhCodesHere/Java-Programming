beb7ca7e35a764d7fc1c414afa1787dcb66d2e62
Yash Kumar <Yashxhere@gmail.com>
2024-01-22 17:12:32 +0530

    Adv. Pattern

---
+++ ZeroOneTriangle.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class ZeroOneTriangle {
+    public static void Zero_One_Triangle(int n){
+        int num = 0;
+        // Outer loop:-
+        for(int i = 1; i <= n; i++){
+            // Inner loop:- 
+            for(int j = 1; j<= i; j++){
+                if ((i+j)%2==0){
+                    num = 0;
+                } else if ((i+j)%2==1){
+                    num = 1;
+                }
+                System.out.print(num);
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number of lines you want in the pattern: ");
+        int n = sc.nextInt();
+        Zero_One_Triangle(n);
+    }
+}
