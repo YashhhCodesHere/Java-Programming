2abbdf60503a268f30c18492a5d0875bef7f8eb8
Yash Kumar <Yashxhere@gmail.com>
2024-01-23 02:24:33 +0530

    Array (Part 1)

---
+++ AddingDataInArray.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class AddingDataInArray {
+    public static void main(String args[]){
+    Scanner sc = new Scanner(System.in);
+
+    int marks[] = new int[100];
+    
+    System.out.print("Enter your Maths marks:");
+    marks[0] = sc.nextInt(); //Maths marks
+    System.out.print("Enter your Chemistry marks:");
+    marks[1] = sc.nextInt(); //Chem marks
+    System.out.print("Enter your Physics marks:");
+    marks[2] = sc.nextInt(); //Physics marks
+    
+    }
+}
