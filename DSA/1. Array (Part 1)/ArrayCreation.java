a4168f5c578a2e1c7c637f47ca7b19d330153cf4
Yash Kumar <Yashxhere@gmail.com>
2024-01-23 02:01:50 +0530

    DSA

---
+++ DSA/1. Array (Part 1)/ArrayCreation.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class ArrayCreation{
+    public static void main(String args[]){
+        int marks[] = new int[50]; // Arrays Created!
+
+        int numbers[] = {1, 2, 3}; // Another syntax to create an array!
+    }
+}
