a0dab8d1e43779446e1130465d7d13a05e098598
Yash Kumar <Yashxhere@gmail.com>
2024-01-26 00:18:48 +0530

    Adv. Pattern

--- SolidRhombusPattern.java
+++ SolidRhombusPattern.java
@@ -9,6 +9,9 @@ public class SolidRhombusPattern {
             for(int j = 1; j <= n; j++){
                 System.out.print("*");
             }
+            for(int j = 1; j <= i; j++){
+                System.out.print(" ");
+            }
             System.out.println();
         }
     }
