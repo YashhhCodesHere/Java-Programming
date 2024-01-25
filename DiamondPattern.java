1604403a212d251d9d7b08351bab203d02799e36
Yash Kumar <Yashxhere@gmail.com>
2024-01-26 01:19:02 +0530

    Adv. Pattern

---
+++ DiamondPattern.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class DiamondPattern {
+    public static void Diamond(int n){
+        
+    }
+    public static void main(String args[]){
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the number of lines you want: ");
+    int n = sc.nextInt();
+    }
+}
--- HollowRhombusPattern.java
+++ HollowRhombusPattern.java
@@ -7,7 +7,7 @@ public class HollowRhombusPattern {
                 System.out.print(" ");
             }
             for(int j = 1; j <= n; j++){
-                if(i == 1 || i == 5 | j == 1 || j == n){
+                if(i == 1 || i == n | j == 1 || j == n){
                     System.out.print("*");
                 } else {
                     System.out.print(" ");
