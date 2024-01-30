f443aae7ef8b6ad0dc0a2e570142fdcf53ab4d73
Yash Kumar <Yashxhere@gmail.com>
2024-01-31 00:09:12 +0530

    Arrays Part 1

--- MaximumNoInArrayByLinearSearching.java
+++ MaximumNoInArrayByLinearSearching.java
@@ -1,11 +1,19 @@
 import java.util.*;
 
 public class MaximumNoInArrayByLinearSearching {
-    public static int LargestNum(int number[]){
-        
+    public static int LargestNum(int numbers[]){
+        int Largest = Integer.MIN_VALUE; // Stores minus infinity or smallest value to the variable Largest!
+
+        for(int i = 0; i < numbers.length; i++){
+            if(numbers[i] > Largest){
+                Largest = numbers[i];
+            }
+        }
+        return Largest;
     }
     public static void main(String args[]){
     int numbers[] = {1, 2, 6, 3, 5};
-
+    LargestNum(numbers);
+    System.out.println("Largest no. in the given array is: " + LargestNum(numbers));
     }
 }
